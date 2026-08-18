ca93ef51c42bc990be17fd96165190c71f62239a
Marvin Kampf <marvin.kampf@uk-erlangen.de>
2026-08-18 16:50:53 +0200

    feat: adds createReferenceTo(Identifier) in ReferenceUtils

--- to-fhir/src/main/java/io/github/dizuker/tofhir/ReferenceUtils.java
+++ to-fhir/src/main/java/io/github/dizuker/tofhir/ReferenceUtils.java
@@ -1,8 +1,10 @@
 package io.github.dizuker.tofhir;
 
 import org.apache.commons.lang3.Validate;
+import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.ResourceType;
 
 /** Utility class for creating FHIR References to resources. */
 public class ReferenceUtils {
@@ -21,5 +23,18 @@ public class ReferenceUtils {
     Validate.notBlank(resource.getId());
     return new Reference(
         resource.getResourceType().name() + "/" + resource.getIdElement().getIdPart());
+  }
+
+  /**
+   * Creates a FHIR Reference to a resource with the given identifier. The reference will be in the
+   * format ResourceType/sha256(identifier.value). The identifier must have a non-blank system and
+   * value set.
+   *
+   * @param identifier an identifier of the FHIR resource to create a reference to
+   * @param resourceType the FHIR ResourceType to include in the reference
+   * @return a Reference pointing to the given resource
+   */
+  public static Reference createReferenceTo(Identifier identifier, ResourceType resourceType) {
+    return new Reference(IdUtils.fromIdentifier(identifier, resourceType));
   }
 }
