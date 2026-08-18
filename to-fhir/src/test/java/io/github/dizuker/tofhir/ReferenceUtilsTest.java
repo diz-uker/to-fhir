456aa9515458aed02d648113ca8dad5a1daf5f50
makampf <1058200+makampf@users.noreply.github.com>
2026-08-18 15:06:58 +0000

    feat: adds createReferenceTo(Identifier) in ReferenceUtils (#84)

--- to-fhir/src/test/java/io/github/dizuker/tofhir/ReferenceUtilsTest.java
+++ to-fhir/src/test/java/io/github/dizuker/tofhir/ReferenceUtilsTest.java
@@ -3,8 +3,10 @@ package io.github.dizuker.tofhir;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.ResourceType;
 import org.junit.jupiter.api.Test;
 
 public class ReferenceUtilsTest {
@@ -42,5 +44,14 @@ public class ReferenceUtilsTest {
     patient.setId("");
 
     assertThrows(Exception.class, () -> ReferenceUtils.createReferenceTo(patient));
+  }
+
+  @Test
+  void testCreateReferenceToIdentifier() {
+    var identifier = new Identifier().setSystem("http://example.com/patient").setValue("12345");
+    var reference = ReferenceUtils.createReferenceTo(identifier, ResourceType.Patient);
+    assertEquals(
+        "Patient/ab0ecec0f0b7f2e7d0034eb57fceee58120d2ecb95d4e05c2613143ae439f652",
+        reference.getReference());
   }
 }
